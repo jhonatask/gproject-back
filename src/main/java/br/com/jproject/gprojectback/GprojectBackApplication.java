0fbe99375b030baaf517dbb82e9fc651b717e026
Jhonata Nazareno <jhonatask@gmail.com>
2024-06-25 11:34:30 -0300

    Criacao da base do projeto de gestao de projetos

---
+++ src/main/java/br/com/jproject/gprojectback/GprojectBackApplication.java
@@ -0,0 +1,13 @@
+package br.com.jproject.gprojectback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GprojectBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GprojectBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/jproject/gprojectback/GprojectBackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jproject.gprojectback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GprojectBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
